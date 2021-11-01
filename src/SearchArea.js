a5fdf1b63bdaf2af5298d36a4a6aa4f313241cbf
Alex Marin Mendez <alexmarinmendez@gmail.com>
2021-11-01 10:05:27 -0500

    momento 5

--- src/SearchArea.js
+++ src/SearchArea.js
@@ -1,46 +1,38 @@
 import React from "react";
 import Videos from "./Videos";
 
-class SearchArea extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            keyword: "",
-            videos: this.props.videos
-        };
-        this.submitHandler = this.submitHandler.bind(this);
-    }
+function SearchArea(props) {
+    const [keyword, setKeyword] = React.useState("");
+    const [videos, setVideos] = React.useState(props.videos);
 
-    submitHandler(e) {
+    function submitHandler(e) {
         e.preventDefault();
-        console.log("Searching for ", this.state.keyword,"...");
-        let filterVideos = this.props.videos.filter(
-            video => video.title.toLowerCase().includes(this.state.keyword.toLowerCase())
+        console.log("Searching for ", keyword,"...");
+        let filterVideos = props.videos.filter(
+            video => video.title.toLowerCase().includes(keyword.toLowerCase())
         );
-        this.setState({videos: filterVideos});
+        setVideos(filterVideos);
     }
 
-    render() {
         return (
             <div className="search-area">
                 <form
-                onSubmit={this.submitHandler}
+                onSubmit={submitHandler}
                 >
                 <label htmlFor="keyword">
                 Search
                 <input
                     id="keyword"
-                    value={this.state.keyword}
+                    value={keyword}
                     placeholder="Search Keyword"
-                    onChange={(e) => this.setState({keyword: e.target.value})}
+                    onChange={(e) => setKeyword(e.target.value)}
                 />
                 </label>
                 <button>Submit</button>
             </form>
-            <Videos videos={this.state.videos} />           
+            <Videos videos={videos} />           
             </div>
         );
-    }
 };
 
 export default SearchArea;
